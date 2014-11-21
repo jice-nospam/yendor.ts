f7d0a79a11d300145680d650d52f2119474fae91
jice-nospam <jice.nospam@gmail.com>
2014-11-21 17:46:03 +0100

    persister can persist arrays like engine.actors directly

--- src/game/persistence.ts
+++ src/game/persistence.ts
@@ -13,9 +13,26 @@ module Game {
 	export interface Persistent {
 		/*
 			Property: className
+			The name of the class must be saved along with the object properties
+			to be able to restore the right type of object when loading from
+			the database
 		*/
 		className: string;
+		/*
+			Function: load
+			Optional custom loading function.
+
+			Parameters:
+			jsonData - parsed json data to load from
+		*/
 		load?: (jsonData: any) => any;
+		/*
+			Function: save
+			Optional custom saving function
+
+			Parameters:
+			key - key to access this object's saved data
+		*/
 		save?: (key: string) => void;
 	}
 
@@ -26,7 +43,6 @@ module Game {
 	export interface Persister {
 		getDataFromKey(key: string): any;
 		loadFromKey(key: string, object?: any): any;
-		loadFromData(jsonData: any, object?: any): any;
 		saveToKey(key: string, object: any);
 		deleteKey(key: string);
 	}
@@ -38,7 +54,11 @@ module Game {
 	export class LocalStoragePersister implements Persister {
 		getDataFromKey(key: string): any {
 			// TODO use a JSON reviver to skip intermediate jsonData step
-			return  JSON.parse(localStorage.getItem(key));
+			var jsonString: string = localStorage.getItem(key);
+			if ( ! jsonString ) {
+				return undefined;
+			}
+			return  JSON.parse(jsonString);
 		}
 
 		saveToKey(key: string, object: any) {
@@ -55,16 +75,25 @@ module Game {
 
 		loadFromKey(localStorageKey: string, object?: any): any {
 			var jsonData: any = this.getDataFromKey(localStorageKey);
+			if (! jsonData ) {
+				return undefined;
+			}
 			return this.loadFromData(jsonData, object);
 		}
 
-		loadFromData(jsonData: any, object?: any): any {
+		private loadFromData(jsonData: any, object?: any): any {
 			if (! object ) {
 				if (! jsonData.className ) {
-					throw new Error("Missing className in json data :" + jsonData);
+					if ( jsonData.length !== undefined ) {
+						// array
+						object = [];
+					} else {
+						throw new Error("Missing className in json data :" + jsonData);
+					}
+				} else {
+					object = Object.create(window[Constants.MAIN_MODULE_NAME][jsonData.className].prototype);
+					object.constructor.apply(object, []);
 				}
-				object = Object.create(window[Constants.MAIN_MODULE_NAME][jsonData.className].prototype);
-				object.constructor.apply(object, []);
 			}
 			if ( object.load ) {
 				object.load(jsonData);
