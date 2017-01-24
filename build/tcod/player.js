b1f7290b1dc5f0636c92af07c4f9f3939520064e
jice <jice.nospam@gmail.com>
2017-01-24 20:39:53 +0100

    remove build files

--- build/tcod/player.js
+++
@@ -1,4 +0,0 @@
-define(["require", "exports"], function (require, exports) {
-    "use strict";
-});
-//# sourceMappingURL=player.js.map
--- build/tcod/world/constants.js
+++
@@ -1,6 +0,0 @@
-define(["require", "exports"], function (require, exports) {
-    "use strict";
-    /** each overworld region represents a square with a side of OVERWORLD_REGION_SIZE tiles */
-    exports.OVERWORLD_REGION_SIZE = 128;
-});
-//# sourceMappingURL=constants.js.map
